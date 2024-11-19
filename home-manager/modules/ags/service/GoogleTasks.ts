95347b4cf2d1dea6745cb6722bc0f045d22cc0a2
Arseniy Knyazev <poseaydone@ya.ru>
2024-11-19 18:14:56 +0500

    Ags fixes

--- home-manager/modules/ags/service/GoogleTasks.ts
+++ home-manager/modules/ags/service/GoogleTasks.ts
@@ -3,141 +3,162 @@ import { fetch, FetchOptions } from "../lib/fetch";
 import GoogleOAuth2Service from "./GoogleOAuth2";
 
 async function fetchWithToken(url: string, options?: FetchOptions = {}) {
-  options["headers"] = {
-    Authorization: `Bearer ${await GoogleOAuth2Service.getAccessToken()}`,
-  };
-  return await fetch(url, options);
+	options["headers"] = {
+		Authorization: `Bearer ${await GoogleOAuth2Service.getAccessToken()}`,
+	};
+	return await fetch(url, options);
 }
 
-type List<T> = {
-  kind: string;
-  etag: string;
-  items: T[];
+export type List<T> = {
+	kind: string;
+	etag: string;
+	items: T[];
 };
 
-type TaskListsItem = {
-  kind: string;
-  id: string;
-  etag: string;
-  title: string;
-  updated: string;
-  selfLink: string;
+export type TaskListsItem = {
+	kind: string;
+	id: string;
+	etag: string;
+	title: string;
+	updated: string;
+	selfLink: string;
 };
 
 export type Task = {
-  kind: string;
-  id: string;
-  etag: string;
-  title: string;
-  updated: string;
-  selfLink: string;
-  position: string;
-  status: "needsAction" | "completed";
-  links: [];
-  webViewLink: string;
+	kind: string;
+	id: string;
+	etag: string;
+	title: string;
+	updated: string;
+	selfLink: string;
+	position: string;
+	status: "needsAction" | "completed";
+	links: [];
+	webViewLink: string;
 };
 
 @register()
 class GoogleTasksService extends GObject.Object {
-  _endpoint = "https://tasks.googleapis.com";
-  _todos: Task[] = [];
-  _completedTodos: Task[] = [];
-  _selectedListId: string | null = null;
-  _availableTaskLists: TaskListsItem[] = [];
+	#endpoint = "https://tasks.googleapis.com";
+	#todos: Task[] = [];
+	#completedTodos: Task[] = [];
+	#selectedListId: string | null = null;
+	#availableTaskLists: TaskListsItem[] = [];
+	#isLoading = true;
 
-  @property()
-  get todos() {
-    return this._todos;
-  }
+	@property()
+	get todos() {
+		return this.#todos;
+	}
 
-  @property()
-  get selectedListId() {
-    return this._selectedListId;
-  }
+	@property(String)
+	get selectedListId() {
+		return this.#selectedListId || "";
+	}
 
-  @property()
-  get availableTaskLists() {
-    return this._availableTaskLists;
-  }
+	set selectedListId(taskListId: string) {
+		this.#selectedListId = taskListId;
+		this.updateTodos();
+	}
 
-  async getTasksLists(): Promise<List<TaskListsItem>> {
-    const res = await fetchWithToken(
-      `${this._endpoint}/tasks/v1/users/@me/lists`,
-    );
-    const data = await res.json();
-    return data;
-  }
+	@property()
+	get availableTaskLists() {
+		return this.#availableTaskLists;
+	}
 
-  async getTasks(listId: string): Promise<List<Task>> {
-    const res = await fetchWithToken(
-      `${this._endpoint}/tasks/v1/lists/${listId}/tasks`,
-      {
-        params: {
-          showCompleted: false,
-        },
-      },
-    );
-    const data = await res.json();
-    return data;
-  }
+	@property(Boolean)
+	get isLoading() {
+		return this.#isLoading;
+	}
 
-  async createTask(title: string): Promise<Task> {
-    const res = await fetchWithToken(
-      `${this._endpoint}/tasks/v1/lists/${this._selectedListId}/tasks`,
-      {
-        method: "POST",
-        body: JSON.stringify({
-          title: title,
-          status: "needsAction",
-        }),
-      },
-    );
-    const data = await res.json();
-    this.updateTodos();
-    return data;
-  }
+	async getTasksLists(): Promise<List<TaskListsItem>> {
+		const res = await fetchWithToken(
+			`${this.#endpoint}/tasks/v1/users/@me/lists`,
+		);
+		const data = await res.json();
+		return data;
+	}
 
-  async checkTask(task: Task): Promise<Task> {
-    const res = await fetchWithToken(
-      `${this._endpoint}/tasks/v1/lists/${this._selectedListId}/tasks/${task.id}`,
-      {
-        method: "PATCH",
-        body: JSON.stringify({
-          status: task.status == "completed" ? "needsAction" : "completed",
-        }),
-      },
-    );
-    const data = await res.json();
-    this.updateTodos();
-    return data;
-  }
+	async getTasks(listId: string): Promise<List<Task>> {
+		const res = await fetchWithToken(
+			`${this.#endpoint}/tasks/v1/lists/${listId}/tasks`,
+			{
+				params: {
+					showCompleted: false,
+				},
+			},
+		);
+		const data = await res.json();
+		return data;
+	}
 
-  constructor() {
-    super();
-    this.initializeData();
-  }
+	async createTask(title: string): Promise<Task> {
+		const res = await fetchWithToken(
+			`${this.#endpoint}/tasks/v1/lists/${this.#selectedListId}/tasks`,
+			{
+				method: "POST",
+				body: JSON.stringify({
+					title: title,
+					status: "needsAction",
+				}),
+			},
+		);
+		const data = await res.json();
+		this.updateTodos();
+		return data;
+	}
 
-  private async updateTodos() {
-    const tasks = await this.getTasks(this._selectedListId!);
-    this._todos = tasks.items.toSorted((task) => parseInt(task.position));
-    this.notify("todos");
-  }
+	async checkTask(task: Task): Promise<Task> {
+		const res = await fetchWithToken(
+			`${this.#endpoint}/tasks/v1/lists/${this.#selectedListId}/tasks/${task.id}`,
+			{
+				method: "PATCH",
+				body: JSON.stringify({
+					status:
+						task.status == "completed"
+							? "needsAction"
+							: "completed",
+				}),
+			},
+		);
+		const data = await res.json();
+		this.updateTodos();
+		return data;
+	}
 
-  private async initializeData() {
-    try {
-      const taskLists = await this.getTasksLists();
-      this._availableTaskLists = taskLists.items;
+	constructor() {
+		super();
+		this.initializeData();
+	}
 
-      if (this._availableTaskLists.length > 0) {
-        this._selectedListId = this._availableTaskLists[7].id;
-        const tasks = await this.getTasks(this._selectedListId);
-        this._todos = tasks.items.toSorted((task) => parseInt(task.position));
-        this.notify("todos");
-      }
-    } catch (error) {
-      console.error("Error initializing Google Tasks data:", error);
-    }
-  }
+	async updateTodos() {
+		this.#isLoading = true;
+		this.notify("is-loading");
+		const tasks = await this.getTasks(this.#selectedListId!);
+		this.#todos = tasks.items;
+		this.notify("todos");
+		this.#isLoading = false;
+		this.notify("is-loading");
+	}
+
+	private async initializeData() {
+		try {
+			const taskLists = await this.getTasksLists();
+			this.#availableTaskLists = taskLists.items;
+			this.notify("available-task-lists");
+
+			if (this.#availableTaskLists.length > 0) {
+				this.#selectedListId = this.#availableTaskLists[0].id;
+				const tasks = await this.getTasks(this.#selectedListId);
+				this.#todos = tasks.items;
+				this.notify("todos");
+			}
+			this.#isLoading = false;
+			this.notify("is-loading");
+		} catch (error) {
+			console.error("Error initializing Google Tasks data:", error);
+		}
+	}
 }
 
 const service = new GoogleTasksService();
--- home-manager/modules/ags/widget/Dashboard/items/Todos.tsx
+++ home-manager/modules/ags/widget/Dashboard/items/Todos.tsx
@@ -2,10 +2,12 @@ import { bind, GObject, Variable } from "astal";
 import { astalify, ConstructProps, Gtk } from "astal/gtk3";
 import TodosService, { Todo } from "../../../service/LocalTodos";
 import icons from "../../../lib/icons";
-import GoogleTasksService, { Task } from "../../../service/GoogleTasks";
+import GoogleTasksService, {
+	Task,
+	TaskListsItem,
+} from "../../../service/GoogleTasks";
 
-
-class ComboBox extends astalify(Gtk.ComboBox){
+class ComboBox extends astalify(Gtk.ComboBox) {
 	static {
 		GObject.registerClass(this);
 	}
@@ -22,89 +24,106 @@ const TodoItem = ({ todo }: { todo: Task }) => {
 		<box spacing={24} hexpand={true} className="todo">
 			<button
 				onClick={() => {
-                    GoogleTasksService.checkTask(todo)
+					GoogleTasksService.checkTask(todo);
 				}}
 			>
 				<icon
 					className="todo__check"
 					icon={
-						todo.status =="completed" ? icons.todo.checkedAlt : icons.todo.unchecked
+						todo.status == "completed"
+							? icons.todo.checkedAlt
+							: icons.todo.unchecked
 					}
 					pixelSize={24}
 				/>
 			</button>
 
-			<label
-				hexpand={true}
-				halign={Gtk.Align.START}
-				label={todo.title}
-			/>
-
-            {/*
-			<button
-				valign={Gtk.Align.CENTER}
-				onClicked={() => {
-					TodosService.remove(idx);
-				}}
-			>
-				<icon
-					className="todo__delete"
-					pixelSize={24}
-					icon={icons.ui.close}
-				/>
-			</button>
-            */}
+			<label hexpand={true} halign={Gtk.Align.START} label={todo.title} />
 		</box>
 	);
 };
 
-
 export default () => {
 	const newTodoText = Variable<string>("");
 
-
 	return (
-		<box vertical className={"todos block"}
-        >
-			<label
-				label={"Todos"}
-				className={"todos__heading"}
-				halign={Gtk.Align.START}
+		<box vertical className={"todos block"}>
+			<ComboBox
+				hexpand={true}
+				setup={(self) => {
+					const model = new Gtk.ListStore();
+					let renderer = new Gtk.CellRendererText();
+					self.pack_start(renderer, true);
+					self.add_attribute(renderer, "text", 1);
+					GoogleTasksService.connect(
+						"notify::available-task-lists",
+						() => {
+							model.set_column_types([
+								GObject.TYPE_STRING,
+								GObject.TYPE_STRING,
+							]);
+							GoogleTasksService.availableTaskLists.forEach(
+								(list: TaskListsItem) => {
+									model.set(
+										model.append(),
+										[0, 1],
+										[list.id, list.title],
+									);
+								},
+							);
+							self.set_model(model);
+						},
+					);
+					self.connect("changed", function (entry) {
+						let [success, iter] = self.get_active_iter();
+						if (!success) return;
+						let selectedListId = model.get_value(iter, 0); // get value
+						GoogleTasksService.selectedListId =
+							selectedListId as string;
+					});
+				}}
 			/>
-			<box className={"todos__input_box"} spacing={24}>
-				<icon icon={icons.todo.checkedAlt} />
-				<entry
-					className={"todos__input"}
-					placeholderText={"New todo..."}
-					onChanged={({ text }) => newTodoText.set(text)}
-					onActivate={(self) => {
-						GoogleTasksService.createTask(newTodoText.get());
-						self.text = "";
-					}}
-				/>
-			</box>
-            {/*
-
 			<stack
 				transitionType={Gtk.StackTransitionType.CROSSFADE}
-				shown={bind(TodosService, "todos").as((t) =>
-					t.length > 0 ? "todos" : "placeholder",
-				)}
+				transitionDuration={200}
+				setup={(self) => {
+					GoogleTasksService.isLoading
+						? (self.shown = "loading")
+						: (self.shown = "todos");
+					self.hook(GoogleTasksService, "notify::is-loading", () => {
+						GoogleTasksService.isLoading
+							? (self.shown = "loading")
+							: (self.shown = "todos");
+					});
+				}}
 			>
-                */}
-				<scrollable className={"todos__scrollable"} name={"todos"}>
-					<box vertical className={"todos__container"}>
-						{bind(GoogleTasksService, "todos").as((t) =>
-							t.map((item) => (
-								<TodoItem todo={item}/>
-							)),
-						)}
+				<box name="todos" vertical>
+					<box className={"todos__input_box"} spacing={24}>
+						<icon icon={icons.todo.checkedAlt} />
+						<entry
+							className={"todos__input"}
+							placeholderText={"New todo..."}
+							onChanged={({ text }) => newTodoText.set(text)}
+							onActivate={(self) => {
+								GoogleTasksService.createTask(
+									newTodoText.get(),
+								);
+								self.text = "";
+							}}
+						/>
 					</box>
-				</scrollable>
-            {/*
-				<box name={"placeholder"}></box>
+					<scrollable className={"todos__scrollable"} name={"todos"}>
+						<box vertical className={"todos__container"}>
+							{bind(GoogleTasksService, "todos").as((t) =>
+								t.map((item) => <TodoItem todo={item} />),
+							)}
+						</box>
+					</scrollable>
+				</box>
+				<box name="loading">
+					<circularprogress />
+				</box>
 			</stack>
-                */}
 		</box>
 	);
 };
